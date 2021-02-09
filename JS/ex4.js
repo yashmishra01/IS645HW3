07b0af5df4c5c07f1e1dbab2892988a97e002c84
yashmishra01 <yam2810@gmail.com>
2021-02-08 23:23:17 -0600

    final1

--- JS/ex4.js
+++ JS/ex4.js
@@ -4,11 +4,11 @@ for (i = 1; i <= 3; i++) {
     let pwd = prompt('Enter a valid password: ');
 
     if (pwd == "secret") {
-        console.log('you entered the correct password after ' + (i - 1) + ' attempts')
+        console.log('you entered the correct password after ' + i + ' attempts')
         break;
     } else if (i < 3) {
         console.log('Wrong Password!');
     } else {
-        console.log('Your account is locked.You failed to enter the correct password after ' + (i - 1) + ' attempts');
+        console.log('Your account is locked.You failed to enter the correct password after ' + i + ' attempts');
     }
 }
